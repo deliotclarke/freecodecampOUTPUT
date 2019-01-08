7518bac7700cab080ae7cc9284931e505771c624
Eliot Clarke <45045397+deliotclarke@users.noreply.github.com>
2019-01-08 13:15:02 -0600

    Update pfft.js
    //Let's redo this with a user input and use console.log instead of return

--- pfft.js
+++ pfft.js
@@ -1,11 +1,10 @@
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
  
- // Your code below this line
   var result = "There was a " + myAdjective + " " + myNoun + " who always " + myVerb + " " + myAdverb + " to the food bowl.";
 
-  // Your code above this line
   return result;
 }
 
-// Change the words here to test your function
 wordBlanks("dog", "big", "ran", "quickly");
+
+
